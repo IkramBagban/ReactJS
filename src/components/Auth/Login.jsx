c5a9d5bd509caee95fea35468143b40bc0521278
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2024-01-24 13:54:57 +0530

    add submit and input change handler

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,28 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Login.module.css";
+import { Link } from "react-router-dom";
 
 function Login() {
+  const [inputValue, setInputValue] = useState({
+    email: "",
+    password: "",
+  });
+
+  const inputChangeHandler = (e) => {
+    const { value, id } = e.target;
+    setInputValue((prev) => ({ ...prev, [id]: value }));
+
+    console.log(inputValue);
+  };
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log(inputValue);
+  };
+
   return (
     <div className={styles.rootContainer}>
-      <form className={styles.container}>
+      <form onSubmit={submitHandler} className={styles.container}>
         <h3>Login</h3>
         <div className={styles.fieldControls}>
           <div className={styles.fieldContainer}>
             <label htmlFor="email">Email</label>
-            <input id="email" />
+            <input
+              value={inputValue.email}
+              id="email"
+              onChange={inputChangeHandler}
+            />
           </div>
           <div className={styles.fieldContainer}>
             <label htmlFor="password">Password</label>
-            <input id="password" />
+            <input
+              value={inputValue.password}
+              id="password"
+              onChange={inputChangeHandler}
+            />
           </div>
           <div className={styles.btnContainer}>
-            <button>Loginn</button>
-            <a
+            <button type="submit">Loginn</button>
+            <Link
               style={{ alignSelf: "center", margin: "1rem 0 0 0" }}
-              href="/signup"
+              to="/signup"
             >
               Signup
-            </a>
+            </Link>
           </div>
         </div>
       </form>
